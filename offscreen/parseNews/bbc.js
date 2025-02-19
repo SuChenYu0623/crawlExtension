0a7715ef2326bc40b68446dab54ff409c904dd61
SuChenYu0623 <F110154121@nkust.edu.tw>
2025-02-20 02:13:36 +0800

    feat: (bbc parseNews) add parseVideoPage

--- offscreen/parseNews/bbc.js
+++ offscreen/parseNews/bbc.js
@@ -25,20 +25,45 @@ function parseImagesWithDesc(dom) {
     .filter((item, index, arr) => arr.map(tmp => tmp.src).indexOf(item.src) === index)
 }
 
+// ---
+function parseVideoPage(doc) {
+  const title = doc.querySelector('[property="og:title"]')?.getAttribute('content')
+  const src = doc.querySelector('[property="og:image"]')?.getAttribute('content')
+  const alt = doc.querySelector('[property="og:image:alt"]')?.getAttribute('content')
+  const images_with_desc = [{ src, alt }]
+
+  let mainDom = doc.querySelector('[data-testid="video-page-container"] [data-testid="video-page-video-section"]')
+  mainDom?.querySelectorAll('time[datetime]')?.forEach(dom => dom.remove())
+  const summary = mainDom?.innerText?.trim()
+  return { title, summary, images_with_desc }
+}
+
 export function parseNews(doc) {
   let item = {}
   try {
-    let title = doc.querySelector('head title')?.innerText
+    let title = ''
+    let summary = ''
+    let images_with_desc = []
 
-    // 清理頁面 dom
-    let mainDom = doc.querySelector('[id="main-content"] article')
-    let dom = cleanHtml(mainDom)
-    // 多圖 和 src
-    let images_with_desc = parseImagesWithDesc(dom)
+    let isVideoPage = doc.querySelector('[data-testid="video-page-container"] [data-testid="video-page-video-section"]')
+    let isNormalPage = doc.querySelector('[id="main-content"] article')
 
-    // 拿不到 summary 直接拿內文
-    dom.querySelectorAll('figure').forEach(_dom => _dom.remove())
-    let summary = dom?.innerText?.trim()
+    if (isVideoPage) {
+      const tmp = parseVideoPage(doc)
+      title = tmp.title
+      summary = tmp.summary
+      images_with_desc = tmp.images_with_desc
+    } else if (isNormalPage) {
+      title = doc.querySelector('head title')?.innerText
+      let mainDom = doc.querySelector('[id="main-content"] article')
+      let dom = cleanHtml(mainDom)
+      // 多圖 和 src
+      images_with_desc = parseImagesWithDesc(dom)
+
+      // 拿不到 summary 直接拿內文
+      dom.querySelectorAll('figure').forEach(_dom => _dom.remove())
+      summary = dom?.innerText?.trim()
+    }
 
     // check news
     if (!title) throw Error('Missing title.');

3e8d969e95a725bb6c374964c50e7f300e6cee54
SuChenYu0623 <F110154121@nkust.edu.tw>
2025-02-20 01:28:28 +0800

    feat: 將監聽器分離出來, 同時加上刪除監聽器避免堆疊滿

--- background/background.js
+++ background/background.js
@@ -5,85 +5,88 @@ chrome.runtime.onInstalled.addListener(async () => {
   console.log("Extension installed!");
 });
 
+async function TaskListener(details) {
+  console.log('增加一次', details.url)
+  // 初步檢查 (不符就直接丟掉)
+  if (details.method !== 'POST' ||
+    !(details.url.includes('127.0.0.1') || details.url.includes('localhost'))
+  ) return
+
+  // 在 MV3 中解析請求 Body
+  const requestBody = details.requestBody ? details.requestBody.raw : null;
+  if (!requestBody) {
+    console.log('details', details)
+    throw Error('Warning Request')
+  }
+  const decodedBody = JSON.parse(
+    String.fromCharCode.apply(null, new Uint8Array(requestBody[0].bytes))
+  );
+
+  // 分配工作
+  const loopUrlPaths = ['/v2/task/loopUrlTask/', '/api/scheduler/loopUrlTask']
+  const isLoopUrlTask = loopUrlPaths.find(pathname => details.url.includes(pathname))
+
+  const collectUrlsPaths = ['/v1/task/collectUrlsTask/', '/api/scheduler/collectUrlsTask']
+  const isCollectUrlsTask = collectUrlsPaths.find(pathname => details.url.includes(pathname))
+
+  // 依照不同task工作
+  if (isLoopUrlTask) {
+    console.log("=== Begin Loop Url ===")
+    const { workType, taskUrls } = decodedBody;
+    console.log('workType', workType, 'taskUrls', taskUrls)
+    let items = []
+    for (let taskUrl of taskUrls) {
+      const { newsId, url, press, postTime } = taskUrl
+      let text = await getNewsHtmlText(url);
+      let { item } = await parseHTMLV2(press, text);
+      console.log('item', item)
+
+      if (!item) {
+        console.group('get item failed.')
+        console.log(taskUrl)
+        console.groupEnd()
+        continue
+      }
+      items.push({
+        ...item,
+        url: url,
+        press: press,
+        newsId: newsId,
+        postTime: postTime
+      })
+      await sleep(1000);
+    }
+    console.log('items', items)
+    let res = await saveNewsItems(items)
+    console.log('res', res)
+  }
+
+  if (isCollectUrlsTask) {
+    console.log("=== Begin Collect Urls ===")
+    const { workType, press } = decodedBody;
+    console.log('workType', workType, press, decodedBody)
+
+
+    let urlsList = await collectUrls(press)
+    console.log('urlsList', urlsList)
+    // 分批存
+    while (1) {
+      if (!urlsList?.length) break
+      let res = await saveNewsUrls(urlsList.splice(urlsList.length - 100))
+      console.log('saveNewsUrls', res)
+      await sleep(1000)
+    }
+  }
+  console.log('Finish Task.')
+  chrome.webRequest.onBeforeRequest.removeListener(TaskListener);
+}
 
 chrome.webRequest.onBeforeRequest.addListener(
-  async (details) => {
-    console.log('增加一次', details.url)
-    // 初步檢查 (不符就直接丟掉)
-    if (details.method !== 'POST' ||
-      !(details.url.includes('127.0.0.1') || details.url.includes('localhost'))
-    ) return
-
-    // 在 MV3 中解析請求 Body
-    const requestBody = details.requestBody ? details.requestBody.raw : null;
-    if (!requestBody) {
-      console.log('details', details)
-      throw Error('Warning Request')
-    }
-    const decodedBody = JSON.parse(
-      String.fromCharCode.apply(null, new Uint8Array(requestBody[0].bytes))
-    );
-
-    // 分配工作
-    const loopUrlPaths = ['/v2/task/loopUrlTask/', '/api/scheduler/loopUrlTask']
-    const isLoopUrlTask = loopUrlPaths.find(pathname => details.url.includes(pathname))
-
-    const collectUrlsPaths = ['/v1/task/collectUrlsTask/', '/api/scheduler/collectUrlsTask']
-    const isCollectUrlsTask = collectUrlsPaths.find(pathname => details.url.includes(pathname))
-
-    // 依照不同task工作
-    if (isLoopUrlTask) {
-      console.log("=== Begin Loop Url ===")
-      const { workType, taskUrls } = decodedBody;
-      console.log('workType', workType, 'taskUrls', taskUrls)
-      let items = []
-      for (let taskUrl of taskUrls) {
-        const { newsId, url, press, postTime } = taskUrl
-        let text = await getNewsHtmlText(url);
-        let { item } = await parseHTMLV2(press, text);
-        console.log('item', item)
-
-        if (!item) {
-          console.group('get item failed.')
-          console.log(taskUrl)
-          console.groupEnd()
-          continue
-        }
-        items.push({
-          ...item,
-          url: url,
-          press: press,
-          newsId: newsId,
-          postTime: postTime
-        })
-        await sleep(1000);
-      }
-      console.log('items', items)
-      let res = await saveNewsItems(items)
-      console.log('res', res)
-    }
-
-    if (isCollectUrlsTask) {
-      console.log("=== Begin Collect Urls ===")
-      const { workType, press } = decodedBody;
-      console.log('workType', workType, press, decodedBody)
-
-
-      let urlsList = await collectUrls(press)
-      console.log('urlsList', urlsList)
-      // 分批存
-      while (1) {
-        if (!urlsList?.length) break
-        let res = await saveNewsUrls(urlsList.splice(urlsList.length - 100))
-        console.log('saveNewsUrls', res)
-        await sleep(1000)
-      }
-    }
-    console.log('Finish Task.')
-  },
+  TaskListener,
   { urls: ["http://127.0.0.1:3000/api/scheduler/loopUrlTask", "http://127.0.0.1:3000/api/scheduler/collectUrlsTask"] }, // 設置監聽範圍
   ["requestBody"]
 );
+
 
 
 // loopUrl
